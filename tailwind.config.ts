ebd24eac3c8a0d18ae934d0eefbded86e8984dab
Alkis Gotovos <alkis.gotovos@gmail.com>
2024-01-11 18:16:19 +0100

    Initial setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,13 +7,26 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    colors: {
+      primary_red: '#F87070',
+      primary_cyan: '#70F3F8',
+      primary_purple: '#D881F8',
+      lightblue: '#D7E0FF',
+      background: '#1E213F',
+      white: '#FFFFFF',
+      offwhite: '#EFF1FA',
+      offblack: '#161932'
     },
+    screens: {
+      tablet: '640px',
+      desktop: '1280px'
+    },
+    backgroundImage: {
+      'oval': 'linear-gradient(315deg, #2E325A 0%, #0E112A 100%)'
+    },
+    boxShadow: {
+      'oval': '50px 50px 100px 0px #121530, -50px -50px 100px 0px #272C5A'
+    }
   },
   plugins: [],
 }
