abf1850298a1f7d1efa8dad82ad926ca3f40d9d5
Alkis Gotovos <alkis.gotovos@gmail.com>
2024-01-19 15:53:02 +0100

    Some refactoring

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { twJoin, twMerge } from 'tailwind-merge'
 import { useEffect, useState } from 'react'
 import { SettingsProvider, useSettings, useSettingsDispatch } from '@/app/SettingsContext'
 import SettingsDialog from '@/app/SettingsDialog'
+import Timer from '@/app/Timer'
 
 enum MenuItem {
   Pomodoro,
@@ -56,126 +57,6 @@ function Menu({ active, onClick, className='' }: { active: MenuItem, onClick: (_
         )}
       </ul>
     </nav>
-  )
-}
-
-function Timer({ durationSeconds, className='' }: { durationSeconds: number, className?: string }) {
-  const [startTime, setStartTime] = useState<number|null>(null)
-  const [remainingSeconds, setRemainingSeconds] = useState(durationSeconds)
-  const [currentDuration, setCurrentDuration] = useState(durationSeconds)
-
-  function isPaused() {
-    return startTime === null
-  }
-
-  function setPaused() {
-    setStartTime(null)
-  }
-
-  function isFinished() {
-    return remainingSeconds === 0
-  }
-
-  function onTimerClick() {
-    if (isFinished()) {
-      setCurrentDuration(durationSeconds)
-      setRemainingSeconds(durationSeconds)
-      setStartTime(Date.now())
-    } else if (isPaused()) {
-      setCurrentDuration(remainingSeconds)
-      setStartTime(Date.now())
-    } else {
-      setPaused()
-    }
-  }
-
-  useEffect(() => {
-    if (startTime !== null && remainingSeconds > 0) {
-      setTimeout(() => {
-        setRemainingSeconds(Math.max(0, currentDuration - (Date.now() - startTime) / 1000))
-      }, 50)
-    }
-  }, [currentDuration, remainingSeconds, startTime])
-
-  function getTimeString(seconds: number) {
-    const roundedSeconds = Math.ceil(seconds)
-    const min = String(Math.floor(roundedSeconds / 60)).padStart(2, '0')
-    const sec = String(roundedSeconds % 60).padStart(2, '0')
-    return `${min}:${sec}`
-  }
-
-  return (
-    <div className={twMerge(
-      `relative flex flex-col shrink-0 w-[300px] h-[300px] rounded-full bg-oval shadow-oval items-center justify-center`,
-      `${className}`,
-    )}>
-      <div className={`absolute top-1/2 transform -translate-y-1/2 w-[248px] h-[248px]`}>
-        <ProgressBar percentage={remainingSeconds / durationSeconds} strokeWidth={6.5} />
-      </div>
-      <div className={`flex flex-col shrink-0 w-[267.7px] h-[267.7px] rounded-full bg-offblack items-center justify-center`}>
-        <div className={`relative text-h1 text-lightblue`}>
-          { getTimeString(remainingSeconds) }
-          { isFinished() && <StartStopButton onClick={onTimerClick} text='restart' /> }
-          { isPaused() && <StartStopButton onClick={onTimerClick} text='start' /> }
-          { !isFinished() && !isPaused() && <StartStopButton onClick={onTimerClick} text='pause' /> }
-        </div>
-      </div>
-    </div>
-  )
-}
-
-function StartStopButton({ className='', onClick, text }: { className?: string, onClick: () => void, text: string }) {
-  return (
-    <button
-      className={`absolute -bottom-7 left-1/2 transform -translate-x-1/2 text-h3 text-lightblue`}
-      onClick={onClick}
-    >
-      {text}
-    </button>
-  )
-}
-
-function ProgressBar({className = '', percentage, strokeWidth = 6}: {
-  className?: string,
-  percentage: number,
-  strokeWidth?: number
-}) {
-  const hexColor = useSettings().colorScheme.hexColor
-
-  if (percentage === 1) {
-    percentage = 0.99999
-  }
-  const angle = percentage * 2 * Math.PI
-
-  const largeAngleFlag = (angle > Math.PI) ? 1 : 0
-  const sweepFlag = (angle > 0) ? 1 : 0
-  const radius = 100
-
-  function dot([[a, b], [c, d]]: number[][], [x, y]: number[]) {
-    return [a * x + b * y, c * x + d * y]
-  }
-
-  function rotationMatrix(angle: number) {
-    const cos = Math.cos(angle)
-    const sin = Math.sin(angle)
-    return [[cos, -sin], [sin, cos]]
-  }
-
-  function addVectors([ax, ay]: number[], [bx, by]: number[]) {
-    return [ax + bx, ay + by]
-  }
-
-  const rotation = rotationMatrix(angle)
-  const [x, y] = addVectors(
-    dot(rotation, [0, -radius]), [radius, radius])
-
-  return (
-    angle !== 0 &&
-    <svg
-      viewBox={`${-0.5 * strokeWidth} ${-0.5 * strokeWidth} ${2 * radius + strokeWidth} ${2 * radius + strokeWidth}`}>
-      <path fill="none" stroke={hexColor} strokeWidth={strokeWidth} strokeLinecap="round" strokeMiterlimit="10"
-            d={`M ${radius} 0 A ${radius} ${radius} 270 ${largeAngleFlag} ${sweepFlag} ${x} ${y}`}/>
-    </svg>
   )
 }
 
