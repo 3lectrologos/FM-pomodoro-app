33608d60f495842abbdc6e2a6dd154577546f7fd
Alkis Gotovos <alkis.gotovos@gmail.com>
2024-01-25 18:14:32 +0100

    Apply start/pause color on active

--- app/types.ts
+++ app/types.ts
@@ -8,21 +8,21 @@ type ColorScheme = {
 export const colorSchemeRed = {
   name: "red",
   bgColor: "bg-primary_red",
-  textColor: "peer-hover:group-[]:text-primary_red",
+  textColor: "peer-hover:group-[]:text-primary_red peer-active:group-[]:text-primary_red",
   hexColor: "#F87070",
 }
 
 export const colorSchemeCyan = {
   name: "cyan",
   bgColor: "bg-primary_cyan",
-  textColor: "peer-hover:group-[]:text-primary_cyan",
+  textColor: "peer-hover:group-[]:text-primary_cyan peer-active:group-[]:text-primary_cyan",
   hexColor: "#70F3F8",
 }
 
 export const colorSchemePurple = {
   name: "purple",
   bgColor: "bg-primary_purple",
-  textColor: "peer-hover:group-[]:text-primary_purple",
+  textColor: "peer-hover:group-[]:text-primary_purple peer-active:group-[]:text-primary_purple",
   hexColor: "#D881F8",
 }
 
